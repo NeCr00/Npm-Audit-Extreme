068ea0a864b4ed4b44ab06a1082405dcc07f240c
Necr00 <giannisxristodou@hotmail.com>
2023-04-02 19:52:53 +0300

    Adding code to audit.py and main.py

--- audit_code.py
+++ audit_code.py
@@ -1,41 +1,58 @@
 import os
 import sys
 import subprocess
+import json
+from log import Log
+
 
 class audit_code:
-    
+
     def __init__(self, project_dir):
         self.project_dir = project_dir
         self.delete_authorization()
-        self.auditable_paths = self.find_auditable_paths() 
-        self.start_audit()
-        
-        
+        self.auditable_paths = self.find_auditable_paths()
+        self.results =self.start_audit()
+
     def delete_authorization(self):
-        for root,dirs,files in os.walk(self.project_dir):
-            for file in files:
-                if file =='.npmrc':
-                    os.remove(os.path.join(root,file))
-    
-    
+        try:
+            for root, dirs, files in os.walk(self.project_dir):
+                for file in files:
+                    if file == '.npmrc':
+                        os.remove(os.path.join(root, file))
+                        Log.info('Removed .npmrc file from %s/%s' %
+                                 (root, file))
+        except Exception as e:
+            Log.error(f"Error deleting .npmrc file: {e}")
+            sys.exit(1)
+
     def find_auditable_paths(self):
         auditable_paths = []
-        for root,dirs,files in os.walk(self.project_dir):
+        for root, dirs, files in os.walk(self.project_dir):
             for file in files:
                 if file == 'package-lock.json':
                     auditable_paths.append(root)
+
+        if len(auditable_paths) == 0:
+            Log.error('No package-lock.json found in %s' % self.project_dir)
+            sys.exit(1)
+
         return auditable_paths
-    
-    
-    def start_audit (self):
-        
+
+    def start_audit(self):
         results_audit = []
-        
         for directory in self.auditable_paths:
             try:
-                result = subprocess.run(['npm', 'audit', '--json'], cwd=directory, capture_output=True, text=True)
+                result = subprocess.run(
+                    ['npm', 'audit', '--json'], cwd=directory, capture_output=True, text=True)
                 results_audit.append(result.stdout)
+                Log.info('Finished auditing %s/package-lock.json' % directory)
             except subprocess.CalledProcessError:
                 print(f'Error running npm audit in {directory}')
-                
-        print(results_audit)
+
+        json_results = [json.loads(result) for result in results_audit]
+        return json_results
+        #print(json.dumps(json_results))
+
+
+    def get_results_json(self):
+        return json.dumps(self.results)
--- main.py
+++ main.py
@@ -1,18 +1,34 @@
-from audit_code import  audit_code
+from results import results
+from audit_code import audit_code
+from vulnerability_checker import vulnerability_checker
+from packages_version_checker import PackageVersionChecker
+from log import Log
+import json
 import sys
 
 
 def getUserArguments():
-    if  len(sys.argv) !=2:
-        print("Please provide a path as an argument.")
+    if len(sys.argv) != 2:
+        Log.error("Usage: python3 main.py <path_to_project>")
         exit(1)
-    
+
     return sys.argv[1]
 
-def main ():
+
+def main():
+    # Get path for project from command line argument
     project_path = getUserArguments()
-    print(project_path)
-    scan = audit_code(project_path)
+    # Get the results from the scanner
+    scanner_result = results(project_path)
+    # Print the results
+    # json_results = json.dumps(scanner_result.processed_results, indent=4)
+
+    auditCode = audit_code(project_path)
+    package_versions = PackageVersionChecker(auditCode.auditable_paths)
+    
+    vulnerability_checker(scanner_result.processed_results,
+                          package_versions.packages)
+ 
 
 
 if __name__ == '__main__':
